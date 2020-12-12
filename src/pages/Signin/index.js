cd3b7ad77ced89f10f9e75b706cf4973c7aea5e9
Belmiro <belmiro.neto@live.com>
2020-12-12 19:35:06 -0300

    Ajustes da pagina de sign-in para utilizar Grid e Box components

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,42 +1,60 @@
 import React from 'react';
-
+import { Grid, Box, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, TextField, Button, Link } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'row',
     height: '100vh',
   },
-  left: {
-    background: 'blue',
-    flexBasis: '58%',
-
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    backgroundImage: 'url(/images/background.jpg)',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'none',
+    padding: theme.spacing(2),
+    textAlign: 'center',
   },
-  right: {
-    background: 'yellow',
-    flexBasis: '42%',
+  avatar: {
+    background: theme.palette.primary.main,
+    marginBottom: theme.spacing(1),
+  },
+  button: {
+    marginTop: theme.spacing(1),
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '64px 32px',
-    alignItems: 'center',
+    margin: theme.spacing(2, 4),
   },
 }));
+
+function CopyRight() {
+  return (
+    <Typography variant="body2" align="center">
+      {'Copyright © '}
+      <a color="inherit" href="https://www.linkedin.com/in/belmiro-neto/">
+        Belmiro Christo Neto
+      </a>{' '}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
 export default function SignIn() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <div className={classes.left}>
+    <Grid container className={classes.root}>
+      <Grid
+        item
+        container
+        md={7}
+        direction="column"
+        justify="center"
+        alignItems="center"
+        className={classes.image}
+      >
         <Typography style={{ color: '#fff', fontSize: 35, lineHeight: '45px' }}>
           <strong>
-            Simplificando a forma de conectar desenvolvedores de Software
+            Simplificando a forma de conectar desenvolvedores de software!
           </strong>
         </Typography>
         <Typography
@@ -51,14 +69,56 @@ export default function SignIn() {
           Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de
           Software
         </Typography>
-      </div>
-      <div className={classes.right}>
-        <form className={classes.form}>
-          <h4>Acesso</h4>
-          <input type="text" />
-          <input type="text" />
-        </form>
-      </div>
-    </div>
+      </Grid>
+      <Grid item md={5}>
+        <Box display="flex" flexDirection="column" alignItems="center" mt={8}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">Acesso</Typography>
+          <form className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              type="password"
+              label="Senha"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Entrar
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link>Esqueceu sua senha?</Link>
+              </Grid>
+              <Grid item>
+                <Link>Não tem uma conta? Registre-se</Link>
+              </Grid>
+            </Grid>
+          </form>
+          <CopyRight />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
