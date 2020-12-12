53b6c2a0c818289f9aac72cc5f59f5653a60733e
Belmiro <belmiro.neto@live.com>
2020-12-12 11:14:49 -0300

    Ajustes e estilização do header

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,18 +1,67 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles, mergeClasses } from '@material-ui/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tollbar from '@material-ui/core/Toolbar';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { Bell } from 'react-feather';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles({
+  appBar: {
+    boxShadow: 'none',
+  },
+  img: {
+    maxHeight: 55,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  userSection: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  button: {
+    marginRight: 10,
+  },
+  bell: {
+    marginRight: 10,
+  },
+});
+
 function Header() {
+  const classes = useStyles();
+
   return (
-    <header className="header">
-      <div className="toolbar">
-        <div className="">
+    <AppBar position="fixed" color="inherit" className={classes.appBar}>
+      <Tollbar>
+        <img src="/images/logo.png" alt="logo" className={classes.img} />
+        <div className={classes.grow}></div>
+        <div className={classes.userSection}>
+          <Button
+            color="primary"
+            variant="contained"
+            className={classes.button}
+          >
+            Novo Post
+          </Button>
+          <SvgIcon className={classes.bell}>
+            <Bell />
+          </SvgIcon>
+          <Avatar alt="Belmiro Christo Neto" src="/" />
+        </div>
+
+        {/* <div className="">
           <a href="/">Conecta Dev</a>
+          <input type="text"></input>
         </div>
         <div className="">
-          <button> Novo Post</button>
+          
           <span>img1</span>
-          <span>img2 Testegit status</span>
-        </div>
-      </div>
-    </header>
+          <span>img2</span>
+        </div> */}
+      </Tollbar>
+    </AppBar>
   );
 }
 export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import './style.css';
+//import './style.css';
+import { makeStyles } from '@material-ui/styles';
 import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Feed from './components/Feed';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  main: {
+    height: '100vh',
+    display: 'flex',
+    width: '1200px',
+    margin: '0 auto',
+  },
+});
+
 function Home() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <Header />
-      <main className="main">
-        <div className="navbar">navbar</div>
-        <div className="feed">Feed</div>
+      <main className={classes.main}>
+        <NavBar />
+        <Feed />
       </main>
     </div>
   );
