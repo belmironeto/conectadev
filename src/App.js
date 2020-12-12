2f5ee385817bd23db9e6571e68686f666436e321
Belmiro <belmiro.neto@live.com>
2020-12-12 18:02:42 -0300

    React-Router6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Home from './pages/Home';
+import SignIn from './pages/Signin';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import theme from './theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/*" element={<h1>Page Not Found - 404!</h1>} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ src/pages/Signin/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LogIn } from 'react-feather';
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1>Login Component</h1>
+    </div>
+  );
+}
